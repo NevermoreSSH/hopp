9f3c19ededb0cf3dadb3e846ec88dc0fb2ae0975
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2023-04-12 12:07:06 +0800

    Update slhostdns.sh

--- slowdns/slhostdns.sh
+++ slowdns/slhostdns.sh
@@ -9,9 +9,9 @@ rm nsdomain
 
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=nevermoressh.tech
-SUB_DOMAIN=${sub}.nevermoressh.tech
-NS_DOMAIN=ns-nevermoressh.tech
+DOMAIN=$(cat /etc/xray/domain)
+SUB_DOMAIN=${sub}.$DOMAIN
+NS_DOMAIN=ns-$DOMAIN
 CF_ID=nevermoressh@gmail.com
 CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
 echo "IP=""$SUB_DOMAIN" >> /var/lib/crot/subdomain.conf
